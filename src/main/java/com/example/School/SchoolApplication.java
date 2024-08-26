c5f41e59356eadbcd9814d3cb4ca8ddeebd8d006
Rohan <rohanpai126@gmail.com>
2024-08-26 16:42:27 +0530

    first commit

--- src/main/java/com/example/School/SchoolApplication.java
+++ src/main/java/com/example/School/SchoolApplication.java
@@ -11,6 +11,8 @@ public class SchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
-
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 }
