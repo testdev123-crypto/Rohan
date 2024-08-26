9d6d553d8cc027a8ca42f9055e2a188fceb83e2a
Rohan <rohanpai126@gmail.com>
2024-08-26 13:33:48 +0530

    third commit

--- src/main/java/com/example/School/SchoolApplication.java
+++ src/main/java/com/example/School/SchoolApplication.java
@@ -3,11 +3,14 @@ package com.example.School;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class SchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
+		System.out.println("Hi");
 	}
-
 }
